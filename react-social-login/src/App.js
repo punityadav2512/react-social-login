5cf8c93b82659b199cc33d9816bb2da8bc7b64ea
punityadav2512 <b520040@iiit-bh.ac.in>
2022-03-09 15:06:56 +0530

    first commit

---
+++ react-social-login/src/App.js
@@ -0,0 +1,27 @@
+
+import './App.css';
+import Login from './pages/Login';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import Post from './pages/Post';
+
+function App() {
+  const user = false;
+  return (
+    <BrowserRouter>
+
+      <div>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/login' element={user ? <Navigate to="/" /> : <Login />} />
+          <Route path='/post/:id' element={user ? <Post /> : <Navigate to="/login" />} />
+
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
